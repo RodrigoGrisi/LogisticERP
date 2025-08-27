443e07144337fe9ad5489e267673430a79afa5bb
Rodrigo Grisi <46337889+RodrigoGrisi@users.noreply.github.com>
2025-08-26 22:49:07 -0300

    feat(api): add POST endpoint for creating drivers
    - Implemented POST /motoristas to register new drivers
    - Persist drivers in the database using EF Core
    - Return success message upon creation

--- Controllers/MotoristasController.cs
+++ Controllers/MotoristasController.cs
@@ -1,6 +1,8 @@
 ﻿using LogisticERP.Context;
+using LogisticERP.Domain;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LogisticERP.Controllers;
 
@@ -16,10 +18,80 @@ public class MotoristasController : ControllerBase
     }
 
     [HttpGet]
-    public IActionResult Get()
+    public ActionResult<IEnumerable<Motorista>> Get()
     {
         var motoristas = _context.Motoristas?.ToList();
-        return Ok(motoristas);
+
+        if(motoristas != null && motoristas.Count == 0)
+        {
+            return NotFound();
+        }
+
+        return motoristas;
     }
+
+    [HttpGet("{id:int}", Name="ObterMotorista")]
+    public ActionResult<Motorista> Get(int id)
+    {
+        var motorista = _context.Motoristas?.FirstOrDefault(m => m.MotoristaID == id);
+        if(motorista == null)
+        {
+            return NotFound("Motorista não encontrado...");
+        }
+        return motorista;
+    }
+
+    [HttpPost]
+    public ActionResult Post(Motorista motorista)
+    {
+
+        if(motorista == null)
+        {
+            return BadRequest();
+        }
+
+        _context.Motoristas.Add(motorista);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("ObterMotorista", 
+            new { id = motorista.MotoristaID  }, motorista);
+
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult EditarDadosMotorista(int id, Motorista motorista)
+    {
+
+        if(id != motorista.MotoristaID)
+        {
+            return BadRequest("não encontramos o motorista indicado.");
+        }
+
+        _context.Entry(motorista).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(motorista);
+
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult DeletarMotorista(int id)
+    {
+
+        var motorista = _context.Motoristas?.FirstOrDefault(m => m.MotoristaID == id);
+
+        if(motorista is null)
+        {
+            return NotFound("Motorista não localizado");
+        }
+
+        _context.Motoristas.Remove(motorista);
+        _context.SaveChanges();
+
+        return Ok("Motorista Deletado com sucesso.");
+
+    }
+
+
 
 }
